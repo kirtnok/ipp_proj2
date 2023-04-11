41a0aa2565ab30445c85c2332019fcb888a1a862
Jakub <jakubkontrik@gmail.com>
2023-04-11 03:34:51 +0200

    Add stack and frame classes

---
+++ frame.py
@@ -0,0 +1,13 @@
+class Frame:
+    def __init__(self):
+        self.vars = {}
+
+    def get_var(self, name):
+        return self.vars[name]
+    
+    def set_var(self, name, value):
+        self.vars[name] = value
+
+    def get_vars(self):
+        return self.vars
+
